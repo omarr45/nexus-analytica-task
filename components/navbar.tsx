74d4050ed22d940c696a12af6d3b72e2847841e5
omarr45 <58887202+omarr45@users.noreply.github.com>
2024-01-26 20:05:29 +0200

    feat: init commit

---
+++ components/navbar.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+
+import { BarChart3 } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+import { ThemeToggle } from './ui/theme-toggle';
+
+const Navbar = () => {
+  return (
+    <nav className='border-b-2 p-2'>
+      <div className='mx-auto flex max-w-5xl items-center justify-between gap-2'>
+        <Link
+          className='mr-auto flex items-center justify-center gap-2 text-3xl font-bold'
+          href='/'
+        >
+          <BarChart3 size={30} strokeWidth={2.5} className='text-primary' />
+          Nexus
+        </Link>
+        <ThemeToggle />
+        <Avatar>
+          {/* <AvatarImage
+            className='scale-125 object-cover'
+            src='/OmarAbdulRahman.jpg'
+          /> */}
+          <AvatarFallback>OA</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
